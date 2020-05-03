d6305c805292729a220b942be5f5c1b0da0a2436
Aga <aga.ltk@gmail.com>
2020-05-03 20:03:54 +0200

    Publiser class correction

--- src/main/java/com/first/spring/bootstrap/BootStrapData.java
+++ src/main/java/com/first/spring/bootstrap/BootStrapData.java
@@ -25,27 +25,33 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book( "Developing Driven Domain",  "123123");
-        eric.getBooks().add(ddd);
-        ddd.getAuthors().add(eric);
-
-        authorRepository.save(eric);
-        bookRepository.save(ddd);
-
-
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book( "J2EE Development without EJB",  "423423");
-        rod.getBooks().add(noEJB);
-        noEJB.getAuthors().add(rod);
 
-        authorRepository.save(rod);
-        bookRepository.save(noEJB);
+        Book ddd = new Book( "Developing Driven Domain",  "123123");
+        Book noEJB = new Book( "J2EE Development without EJB",  "423423");
 
         Publisher john = new Publisher("John", "Scary 2", "New York", "Big and nice", "34-098");
         publisherRepository.save(john);
 
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        ddd.setPublisher(john);
+        john.getBooks().add(ddd);
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        noEJB.setPublisher(john);
+        john.getBooks().add(noEJB);
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(john);
+
     System.out.println("Start in BootStrap");
     System.out.println("Number of books " + bookRepository.count());
-    System.out.println("Number of publishers " + publisherRepository.count());
+    System.out.println("Number of publisher books " + john.getBooks().size());
     }
 }
